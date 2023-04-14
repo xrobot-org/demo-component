ce8c8dd78e15d5e899e4d0a02dcacf4309b170ff
xiao <2592509183@qq.com>
2023-04-14 20:52:34 +0800

    fix cmake

--- info.cmake
+++ info.cmake
@@ -1,7 +1,9 @@
 CHECK_SUB_ENABLE(MODULE_ENABLE component)
-
 if(${MODULE_ENABLE})
     file(GLOB CUR_SOURCES "${USER_DIR}/component/${SUB_DIR}/*.cpp")
+
+    set(CUR_INCLUDES ${USER_DIR}/component/${SUB_DIR})
+
     SUB_ADD_SRC(CUR_SOURCES)
-    SUB_ADD_INC(SUB_DIR)
+    SUB_ADD_INC(CUR_INCLUDES)
 endif()
